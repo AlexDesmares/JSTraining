60e4d9d78ae7234bd4f15a7b64d4357423846073
AlexandreDESMARES <alexandre.desmares@arintech.io>
2019-06-21 15:32:36 +0200

    JSfirstJSCode01.js modified

---
+++ firstJSCode01.js
@@ -0,0 +1,27 @@
+console.log("Il était une Fois, le JavaScript");
+console.log("Bonjour en JavaScript !");
+// Cette ligne est un commentaire : elle n'est pas exécutée
+console.log("Faisons quelques calculs.");
+console.log(4 + 7);
+console.log(12 / 0);
+console.log("Au revoir !");
+/* Un commentaire 
+sur plusieurs
+lignes */
+ 
+// Un commentaire sur une seule ligne
+
+// Nom et age
+console.log("Nom : Alex");
+console.log("Age : 37 ans -- sans commentaire s'il vous plait");
+
+// Un peu de calcul
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+
+// Tests
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
