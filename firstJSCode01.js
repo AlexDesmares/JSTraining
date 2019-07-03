ad1383dbd84605c48a42f8729a5ee0977c57ee87
AlexandreDESMARES <alexandre.desmares@arintech.io>
2019-07-03 12:03:27 +0200

    minor modification

--- firstJSCode01.js
+++ firstJSCode01.js
@@ -358,18 +358,18 @@ let nombre;
 for (nombre = 1; nombre <= 100; nombre ++){
   let div3 = nombre % 3;
   let div5 = nombre % 5;
-  if ((div3 = 0) && (div5 != 0)){
-    console.log("Fizz")
+  if ((div3 === 0) && (div5 !== 0)){
+    console.log("Fizz");
   }
-  else if ((div3 != 0) && (div5 = 0)){
-    console.log("Buzz")
+  else if ((div3 !== 0) && (div5 === 0)){
+    console.log("Buzz");
   }
-  else if ((div3 = 0) && (div5 = 0)){
-    console.log("FizzBuzz")
+  else if ((div3 === 0) && (div5 === 0)){
+    console.log("FizzBuzz");
   }
   else {
-    console.log(`${nombre}`)
+    console.log(`${nombre}`);
   }
 }
 
-console.log("Fin du programme")
+console.log("Fin du programme");
