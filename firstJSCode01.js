8d20aea1ef53e3d15986ec9fc67833dbdf560b96
AlexandreDESMARES <alexandre.desmares@arintech.io>
2019-07-04 15:19:32 +0200

    programmation type objets

--- firstJSCode01.js
+++ firstJSCode01.js
@@ -498,3 +498,170 @@ console.log(calculer(4, "-", 6));  // -2
 console.log(calculer(2, "*", 0));  // 0
 console.log(calculer(12, "/", 0)); // Infinity
 
+// objects
+const stylo = {
+  type: "bille",
+  couleur: "bleu",
+  marque: "Bic"
+};
+
+console.log(stylo.type); // "bille"
+console.log(stylo.couleur); // "bleu"
+console.log(stylo.marque); // "Bic"
+
+//
+
+const stylo = {
+  type: "bille",
+  couleur: "bleu",
+  marque: "Bic"
+};
+
+// "J'écris avec un stylo bille bleu de marque Bic"
+console.log(`J'écris avec un stylo ${stylo.type} ${stylo.couleur} de marque ${stylo.marque}`);
+
+//
+
+const stylo = {
+  type: "bille",
+  couleur: "bleu",
+  marque: "Bic"
+};
+
+// Modification de la propriété "couleur"
+stylo.couleur = "rouge";
+
+// "J'écris avec un stylo bille rouge de marque Bic"
+console.log(`J'écris avec un stylo ${stylo.type} ${stylo.couleur} de marque ${stylo.marque}`);
+
+//
+
+const stylo = {
+  type: "bille",
+  couleur: "bleu",
+  marque: "Bic"
+};
+
+// Ajout de la propriété "prix"
+stylo.prix = "2.5";
+
+// "Mon stylo coûte 2.5 euros"
+console.log(`Mon stylo coûte ${stylo.prix} euros`);
+
+//Aurora game
+
+const aurora = {
+  nom: "Aurora",
+  sante: 150,
+  force: 25
+};
+
+// "Aurora a 150 points de vie et 25 en force"
+console.log(`${aurora.nom} a ${aurora.sante} points de vie et ${aurora.force} en force`);
+
+console.log("Aurora est blessée par une flèche");
+aurora.sante = aurora.sante - 20;
+
+console.log("Aurora trouve un bracelet de force");
+aurora.force = aurora.force + 10;
+
+// "Aurora a 130 points de vie et 35 en force"
+console.log(`${aurora.nom} a ${aurora.sante} points de vie et ${aurora.force} en force`);
+
+//intégration d'unefonction
+
+const aurora = {
+  nom: "Aurora",
+  sante: 150,
+  force: 25
+};
+
+// Renvoie la description du personnage
+function decrire(personnage) {
+  return `${personnage.nom} a ${personnage.sante} points de vie et ${personnage.force} en force`;
+}
+
+// "Aurora a 150 points de vie et 25 en force"
+console.log(decrire(aurora));
+
+//integration d'une methode
+
+const aurora = {
+  nom: "Aurora",
+  sante: 150,
+  force: 25,
+
+  // Renvoie la description du personnage
+  decrire() {
+    return `${this.nom} a ${this.sante} points de vie et ${this.force} en force`;
+  } //la methode est une fonction integree comme une propriete d'un objet
+  // ici decrire est une proprietee integrant une fonction
+  //le mot cle this. represente l'objet pour lequel la fonction est appelee
+};
+
+// "Aurora a 150 points de vie et 25 en force"
+console.log(aurora.decrire());
+
+//exercice aurora xp
+
+const aurora = {
+  nom: "aurora",
+  sante: 150,
+  force: 25,
+  xp: 0,
+  
+  decrire () {
+    return `${this.nom} a ${this.sante} points de vie, ${this.force} en force et ${this.xp} points d'experience`;
+  }
+};
+
+// "Aurora a 150 points de vie, 25 en force et 0 points d'expérience"
+console.log(aurora.decrire());
+
+console.log("Aurora apprend une nouvelle compétence");
+aurora.xp += 15;
+
+// "Aurora a 150 points de vie, 25 en force et 15 points d'expérience"
+console.log(aurora.decrire());
+
+// exercice chien
+const chien = {
+  nom: "Crockdur",
+  race: "mâtin de Naples",
+  taille: "75",
+  aboyer() {
+    return "Grrr ! Grrr !"
+  }
+};
+
+// "Crockdur est un mâtin de Naples mesurant 75 cm"
+console.log(`${chien.nom} est un ${chien.race} mesurant ${chien.taille} cm`);
+
+// "Tiens, un chat ! Crockdur aboie : Grrr ! Grrr !"
+console.log(`Tiens, un chat ! ${chien.nom} aboie : ${chien.aboyer()}`);
+
+
+// Modélisation d'un compte bancaire
+const compte = {
+  titulaire: "Alex",
+  solde: 0,
+
+  // Ajoute un montant au solde
+  crediter(montant) {
+    this.solde += montant;
+  },
+
+  // Renvoie la description du compte
+  decrire() {
+    return `titulaire: ${this.titulaire}, solde: ${this.solde}`;
+  }
+};
+
+// "titulaire: Alex, solde: 0"
+console.log(compte.decrire());
+
+compte.crediter(250);
+compte.crediter(-80);
+
+// "titulaire: Alex, solde: 170"
+console.log(compte.decrire());
